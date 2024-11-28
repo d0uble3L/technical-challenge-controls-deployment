eff8abe5b007e05b84418db43239442229a2a86f
Michael T <60906210+d0uble3L@users.noreply.github.com>
2024-11-27 18:04:21 -0600

    Update main.tf

--- Part3_CI_CD_Pipeline_Setup/Terraform/main.tf
+++ Part3_CI_CD_Pipeline_Setup/Terraform/main.tf
@@ -7,6 +7,7 @@ provider "aws" {
 resource "aws_instance" "web_server" {
   ami           = "ami-0c55b159cbfafe1f0" # Example AMI ID
   instance_type = "t2.micro"
+  root_block_device.encrypted = "true"
 
   # Add tags to the instance
   tags = {
